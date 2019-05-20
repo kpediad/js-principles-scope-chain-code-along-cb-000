7311c3bc315fb008d53a16646ca8701f80ce4f73
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-20 10:09:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-/* Your code goes here */
-var customerName = 'bob';
+var customerName = 'bob'
+const leastFavoriteCustomer = 'sour fred'
 
 function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase();
+  return customerName = customerName.toUpperCase()
 }
 
-function setBestCustomer() {
-  bestCustomer = 'not bob';
-}
-
-function overWriteBestCustomer() {
-  bestCustomer = 'maybe bob';
-}
-
-const leastFavoriteCustomer = 'Dick';
-
-function changeLeastFavoriteCustomer() {
-  leastFavoriteCustomer = 'not Dick';
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'sour fred'
 }
 
 
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
 
-function attemptTwoFavoriteCustomers() {
-  //let favoriteCustomer;
-  //throw new SyntaxError('Flatiron Fix your tests, before wasting our time!', 'someFile.js', 10);
-  //let favoriteCustomer;
-  let favoriteCustomer = 'bar'
-  let favoriteCustomer = 'not bar'
+function overWriteBestCustomer(newFavorite){
+  bestCustomer = newFavorite
+}
+
+function congratulateCustomer(){
+  return `congrats ${favoriteCustomer}`
+}
+
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = 'bar'
+    let favoriteCustomer = 'not bar'
 }
